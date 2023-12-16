08541004fb4c18ea5d0d10e4cef664d7ac8efb1a
Fadhlan Putra G <fputragusna18@gmail.com>
2023-12-16 14:52:37 +0700

    first

---
+++ resources/views/formpmb.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PMB</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+  </head>
+  <body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <form method="post" action="">
+    <div class="mb-3">
+        <label for="nama" class="form-label">Nama</label>
+        <input type="text" class="form-control" id="nama" placeholder="">
+        <label for="alamatktp" class="form-label">Alamat KTP</label>
+        <textarea class="form-control" id="alamatktp" rows="3"></textarea>
+        <label for="alamat" class="form-label">Alamat saat ini</label>
+        <textarea class="form-control" id="alamat" rows="3"></textarea>
+        <label for="kecamatan" class="form-label">Kecamatan</label>
+        <input type="text" class="form-control" id="kecamatan" placeholder="">
+        <label for="kabupaten" class="form-label">Kabupaten</label>
+        <input type="text" class="form-control" id="kabupaten" placeholder="">
+        <label for="provinsi" class="form-label">Provinsi</label>
+        <input type="text" class="form-control" id="provinsi" placeholder="">
+        <label for="email" class="form-label">Email</label>
+        <input type="email" class="form-control" id="email" placeholder="">
+    </div>
+    </form>
+
+
+  </body>
+
+
+</html>
---
+++ resources/views/login.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    
+    <title>Login</title>
+
+</head>
+<body>
+    <div id="logsin">
+        <div class="signin">
+            <img src="Image/Group 2014.png" alt="" style="position: absolute; left: 47%; z-index: 99; height: 94%;">
+            <div class="split left">
+                <div class="centered">
+                    <h2>login</h2>
+
+                    <form action="" method="POST" class="signin">
+                        <p>Email </p>
+                        <div class="input-group">
+                            <input type="text" placeholder="email" name="email" value="masukkan email" required>
+                        </div>
+                        <p>Password </p>
+                        <div class="input-group">
+                            <input type="password" placeholder="Password" name="password" value="password" required>
+                        </div>
+                        <div style="text-align: end;"><a href="" style="text-decoration: none; color: darkgray; font-family: 'Poppins'; font-size: 75%;">Lupa password?</a></div>
+                        <div class="input-group" style="text-align: center;">
+                            <button name="submit" class="btn" type="submit">Log In</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="split right">
+                <img src="Image/Rectangle 1040.png" alt="" style="position: absolute; left: 40%;">
+            </div>
+        </div>
+    </div>
+</body>
+</html>
